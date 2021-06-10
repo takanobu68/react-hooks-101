fe041ec61ec92a4281244050bcf83c42c4771fb9
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-10 12:30:51 +0900

    useStateの使い方

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>this is atemplate for React</div>;
+  const [count, setCount] = useState(0);
+
+  const onClickAdd = () => setCount(count + 1);
+  const onClickDecrement = () => setCount(count - 1);
+
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
+  const mult = () => setCount((prev) => prev * 2);
+
+  const division = () => count % 3 === 0 && setCount(count / 3);
+  return (
+    <>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={onClickAdd}>増加</button>
+        <button onClick={onClickDecrement}>減少</button>
+      </div>
+      <br />
+      <div>
+        <button onClick={increment}>増やす</button>
+      </div>
+      <br />
+      <div>
+        <button onClick={() => setCount(0)}>リセット</button>
+        <button onClick={mult}>✖2</button>
+        <br />
+        <button onClick={division}>3の倍数で割れる値の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
