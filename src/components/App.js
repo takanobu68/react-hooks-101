6d9b9ba11d830272aaa603a655ca6db2d1ec7620
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-10 17:34:22 +0900

    implement all events deletion deature

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,16 @@ const App = () => {
     setBody('');
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      '全てのイベントを本当に削除しても良いですか？'
+    );
+    result && dispatch({ type: 'DELETE_ALL_EVENTS' });
+  };
+
+  const unCreatable = title === '' || body === '';
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -45,10 +55,20 @@ const App = () => {
           />
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          disabled={unCreatable}
+          onClick={addEvent}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          disabled={state.length === 0}
+          onClick={deleteAllEvents}
+        >
+          全てのイベントを削除する
+        </button>
       </form>
 
       <h4>イベント一覧</h4>
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Events = ({ dispatch, event }) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
-    dispatch({
-      type: 'DELETE_EVENT',
-      id,
-    });
+    const result = window.confirm(
+      `イベント(id=${id})を本当に削除しても良いですか？`
+    );
+    result &&
+      dispatch({
+        type: 'DELETE_EVENT',
+        id,
+      });
   };
   return (
     <tr>
